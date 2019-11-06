4540e34b32015bfc53d0b8f91613ac043e88941c
unknown <huangxinping@cyou-inc.com>
2019-11-06 15:21:59 +0800

    update back dir

--- clock-tinker/src/main/java/com/longwen/clock/BrokenClockTinkerModule.java
+++ clock-tinker/src/main/java/com/longwen/clock/BrokenClockTinkerModule.java
@@ -1,4 +1,4 @@
-package com.alibaba.jvm.sandbox.demo;
+package com.longwen.clock;
 import com.alibaba.jvm.sandbox.api.Information;
 import com.alibaba.jvm.sandbox.api.Module;
 import com.alibaba.jvm.sandbox.api.ProcessController;
@@ -22,7 +22,7 @@ public class BrokenClockTinkerModule implements Module {
     public void repairCheckState() {
 
         new EventWatchBuilder(moduleEventWatcher)
-                .onClass("com.longwen.clock.Clock")
+                .onClass("com.longwen..Clock")
                 .onBehavior("checkState")
                 .onWatch(new AdviceListener() {
 
